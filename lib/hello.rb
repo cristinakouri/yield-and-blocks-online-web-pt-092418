91eee45370db122388d20438e78ba70b015a4582
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-10-02 23:26:31 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,7 @@
 require 'pry' 
 
 def hello_t(array)
-  block_given? ? array.each {|item| yield(item)} : puts "Hey! No block was given!" 
+  block_given? ? array.each {|item| yield(item)} : (puts "Hey! No block was given!") 
 end
 
 # call your method here!
